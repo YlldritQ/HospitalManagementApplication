4aab305b7bc1046aff8cd6607740aba5917b85e4
YlldritQ_03 <yq68447@ubt-uni.net>
2024-08-18 21:52:25 +0200

    Project initialization and user entities

---
+++ backend/backend/Core/Dtos/Auth/LoginServiceResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Core.Dtos.Auth
+{
+    public class LoginServiceResponseDto
+    {
+        public string newToken { get; set; }
+        public UserInfoResult UserInfo { get; set; }
+    }
+}
